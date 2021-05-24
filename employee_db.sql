789c29b2d1e5b7c69ee7da069e404d213635f98f
Juan Vazquez <ehulises28@hotmail.com>
2021-05-23 19:27:44 -0700

    fixed sechema

--- employee_db.sql
+++ employee_db.sql
@@ -39,34 +39,3 @@ manager_id INT,
 PRIMARY KEY (id)
 
 );
-
-
-
-INSERT INTO department (name) 
-VALUES ('Sales');
-INSERT INTO department (name) 
-VALUES ('Stock');
-INSERT INTO department (name) 
-VALUES ('Management');
-
-INSERT INTO role (title, salary, department_id)
- VALUES ('Sales Associate', 40000, 1);
-INSERT INTO role (title, salary, department_id) 
-VALUES ('Stock Supervisor', 45000, 2);
-INSERT INTO role (title, salary, department_id) 
-VALUES ('Floor Supervisor', 50000, 3);
-INSERT INTO role (title, salary, department_id) 
-VALUES ('Assistant Manager', 55000, 4);
-INSERT INTO role (title, salary, department_id) 
-VALUES ('General Manager', 60000, 5);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-ValUES ('Jose', 'Ramirez', 5, 5)
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-ValUES ('Alan', 'Uribe', 4, 5)
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-ValUES ('Hector', 'Jeronimo', 3, 5)
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-ValUES ('Brian', 'Ruvalcaba', 2, 5)
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-ValUES ('Aron', 'velazquez', 1, 5)
