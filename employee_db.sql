cc8d56d5437aae0d0883ad746371d865ed9eca56
Juan Vazquez <ehulises28@hotmail.com>
2021-05-23 19:26:09 -0700

    updated schema and finseh index.js

--- employee_db.sql
+++ employee_db.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF EXISTS employee_db:
+DROP DATABASE IF EXISTS employee_db;
 --create employee_db database
-CREATE DATABASE employee_db
+CREATE DATABASE employee_db;
 
 USE employee_db;
 
 CREATE TABLE department(
-     id INTEGER NOT NULL AUTO_INCREMEN
+     id INT NOT NULL AUTO_INCREMENT,
      name VARCHAR(50) NOT NULL,
      PRIMARY KEY (id)
 
-)
+);
 CREATE TABLE role (
 
 id INT NOT NULL AUTO_INCREMENT,
@@ -28,9 +28,9 @@ CREATE TABLE employee (
 
 id INT NOT NULL AUTO_INCREMENT,
 
-first_name VARCHAR(30) NOT NULL,
+first_name VARCHAR(50) NOT NULL,
 
-last_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
 
 role_id INT NOT NULL,
 
@@ -39,3 +39,34 @@ manager_id INT,
 PRIMARY KEY (id)
 
 );
+
+
+
+INSERT INTO department (name) 
+VALUES ('Sales');
+INSERT INTO department (name) 
+VALUES ('Stock');
+INSERT INTO department (name) 
+VALUES ('Management');
+
+INSERT INTO role (title, salary, department_id)
+ VALUES ('Sales Associate', 40000, 1);
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Stock Supervisor', 45000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Floor Supervisor', 50000, 3);
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Assistant Manager', 55000, 4);
+INSERT INTO role (title, salary, department_id) 
+VALUES ('General Manager', 60000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ValUES ('Jose', 'Ramirez', 5, 5)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ValUES ('Alan', 'Uribe', 4, 5)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ValUES ('Hector', 'Jeronimo', 3, 5)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ValUES ('Brian', 'Ruvalcaba', 2, 5)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ValUES ('Aron', 'velazquez', 1, 5)
--- index.js
+++ index.js
@@ -1,27 +1,297 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-// inquirer prompt with questions
+
 const connection = mysql.createConnection({
-  hots: "localhost",
+  host: "localhost",
+
+  // Your port; if not 3306
   port: 3306,
+
+  // Your username
   user: "root",
+
+  // Your password
   password: "password",
-  database: "",
+  database: "employee_db",
 });
-const startQuestions = () => [
-  {
-    type: "list",
-    message: "what would you like to do?",
-    choices: [
-      "View all employees",
-      "View all employees by department",
-      "View employees by role",
-      "Add an employee",
-      "Add a role",
-      "Add a department",
-      "Update employee role",
-      "Quit",
-    ],
-    name: "choices",
-  },
-];
+
+connection.connect((err) => {
+  if (err) throw err;
+  startQuestions();
+});
+
+const startQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Employees",
+          "View All Roles",
+          "Add Department",
+          "Remove Department",
+          "Add Employee",
+          "Remove Employee",
+          "Add Role",
+          "Update Employee Role",
+          "Quit",
+        ],
+        name: "start",
+      },
+    ])
+    .then((answers) => {
+      switch (answers.start) {
+        case "View All Departments":
+          showDepartment();
+          break;
+        case "View All Employees":
+          showEmployees();
+          break;
+        case "View All Roles":
+          showRoles();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Remove Department":
+          deleteDepartment();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Remove Employee":
+          deleteEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Update Employee Role":
+          updateRole();
+          break;
+        case "Quit":
+          console.log("Here is your new team");
+          connection.end();
+        default:
+          break;
+      }
+    });
+};
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Whats the first name of the Employee?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Whats the last name?",
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "Enter the Employee's role id",
+      },
+      {
+        type: "input",
+        name: "managerid",
+        message: "Enter the Employee's manager id",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answers.firstName,
+          last_name: answers.lastName,
+          role_id: answers.role,
+          manager_id: answers.managerid,
+        },
+        (err, res) => {
+          if (err) throw err;
+          startQuestions();
+        }
+      );
+    });
+};
+
+const deleteEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "which employee id would you like to delete?",
+        name: "deleteEmployee",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "DELETE FROM employee WHERE id = ?",
+        answers.deleteEmployee,
+        (err, res) => {
+          if (err) throw err;
+          showEmployees();
+          console.log("\n");
+          startQuestions();
+        }
+      );
+    });
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Whats the name of the Department that you would like to add?",
+        name: "department",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "INSERT INTO department SET name = ?",
+        answers.department,
+        (err, res) => {
+          if (err) throw err;
+          showDepartment();
+
+          console.log("\n");
+
+          startQuestions();
+        }
+      );
+    });
+};
+
+const deleteDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "which department id would you like to delete?",
+        name: "deleteDepartment",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "DELETE FROM department WHERE id = ?",
+        answers.deleteDepartment,
+        (err, res) => {
+          if (err) throw err;
+          showDepartment();
+          console.log("\n");
+          startQuestions();
+        }
+      );
+    });
+};
+
+const addRole = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastName",
+      },
+      {
+        type: "list",
+        message: "What is the employee's role",
+        choices: [
+          "Sales Associate",
+          "Stock Supervisor",
+          "Floor Supervisor",
+          "Assistant Manager",
+          "General Manager",
+        ],
+        name: "role",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          first_name: answers.firstName,
+          last_name: answers.lastName,
+          role: answers.role,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          startQuestions();
+        }
+      );
+    });
+};
+
+const updateRole = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Which employee's role would you like to update",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What should the role be updated to",
+        name: "role",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "UPDATE employee SET role_id = ?  WHERE id = ?;",
+        [answers.role, answers.name],
+        (err, res) => {
+          if (err) throw err;
+          showEmployees();
+          startQuestions();
+        }
+      );
+    });
+};
+
+const showRoles = () => {
+  connection.query("SELECT * FROM role", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+  });
+  startQuestions();
+};
+
+const showEmployees = () => {
+  console.log("Showing all employees...\n");
+  connection.query(
+    `
+  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department_name, role.salary 
+  FROM employee 
+  JOIN role 
+  ON role.id = employee.role_id 
+  JOIN department 
+  ON role.department_id = department.id`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      startQuestions();
+    }
+  );
+};
+
+const showDepartment = () => {
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    console.log("\n");
+    console.table(res);
+  });
+  startQuestions();
+};
